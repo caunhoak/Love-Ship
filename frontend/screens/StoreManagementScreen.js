dad03bb855372fc3efa0f4fe270f5acda81bd283
caunhoak <tanndgcd191158@fpt.edu.vn>
2024-03-22 15:43:14 +0700

    fix to fix

---
+++ frontend/screens/StoreManagementScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const StoreManagementScreen = () => {
+  return (
+    <View>
+      <Text>StoreManagementScreen</Text>
+    </View>
+  )
+}
+
+export default StoreManagementScreen
