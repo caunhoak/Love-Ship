4db57fe8a5738b44e3814b83154c4b0823301887
caunhoak <tanndgcd191158@fpt.edu.vn>
2024-03-27 08:54:22 +0700

    test

--- backend/app.js
+++ backend/app.js
@@ -1,51 +1,51 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const dbConfig = require('./config/dbConfig');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dbConfig = require("./config/dbConfig");
 
 // Import các route và controllers
-const authRoutes = require('./routes/authRoutes');
-const storeRoutes = require('./routes/storeRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const productRoutes = require('./routes/productRoutes');
+const authRoutes = require("./routes/authRoutes");
+const storeRoutes = require("./routes/storeRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
 // Kết nối đến cơ sở dữ liệu MongoDB
-dbConfig.once('open', () => {
-    console.log('MongoDB connected');
+dbConfig.once("open", () => {
+  console.log("MongoDB connected");
 });
-dbConfig.on('error', err => {
-    console.error('MongoDB connection error:', err);
+dbConfig.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
 });
 
 // Sử dụng middleware để xử lý các yêu cầu JSON
 app.use(bodyParser.json());
 
 // Sử dụng các route đã được định nghĩa
-app.use('/api/auth', authRoutes);
-app.use('/api/stores', storeRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/products', productRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/stores", storeRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/products", productRoutes);
 
 // Xử lý lỗi cho các route không tồn tại hoặc lỗi khác
 app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 // Khởi động server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
