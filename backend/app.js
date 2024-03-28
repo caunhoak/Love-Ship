7e29348fc2001324fe5beb7a90eb29758b438905
caunhoak <tanndgcd191158@fpt.edu.vn>
2024-03-29 00:34:25 +0700

    modified port

--- backend/app.js
+++ backend/app.js
@@ -45,7 +45,7 @@ app.use((error, req, res, next) => {
 });
 
 // Khởi động server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
