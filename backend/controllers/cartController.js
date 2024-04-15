cd33b002887a11e04220e30700cef9d14072161a
caunhoak <tanndgcd191158@fpt.edu.vn>
2024-04-15 11:37:09 +0700

    add cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -4,8 +4,6 @@ const Cart = require("../models/Cart");
 exports.createCart = async (req, res) => {
   try {
     const { userId } = req.body;
-    console.log(userId);
-
     // Check if userId exists in request body
     if (!userId) {
       return res
@@ -21,26 +19,35 @@ exports.createCart = async (req, res) => {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
   }
+
+  // try {
+  //   const { userId } = req.body;
+  //   // Check if userId exists in request body
+  //   if (!userId) {
+  //     return res
+  //       .status(400)
+  //       .json({ message: "Missing required parameter: userId" });
+  //   }
+
+  //   // Try to find an existing cart for the user
+  //   let cart = await Cart.findOne({ user_id: userId });
+
+  //   // If no cart found, create a new one
+  //   if (!cart) {
+  //     cart = await Cart.create({ user_id: userId });
+  //     return res.status(201).json({
+  //       message: "Cart created successfully",
+  //       cart: cart,
+  //     });
+  //   }
+
+  //   // If cart found, return it
+  //   res.status(200).json({
+  //     message: "Existing cart retrieved successfully",
+  //     cart: cart,
+  //   });
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(500).json({ message: "Internal server error" });
+  // }
 };
-
-// // Controller to create a new cart
-// exports.createCart = async (req, res) => {
-//   try {
-//     const { userId } = req.body;
-
-//     // Check if userId exists in request body
-//     if (!userId) {
-//       return res
-//         .status(400)
-//         .json({ message: "Missing required parameter: userId" });
-//     }
-
-//     // Create a new cart
-//     const cart = await Cart.create({ user_id: userId });
-
-//     res.status(201).json({ message: "Cart created successfully", cart: cart });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// };
